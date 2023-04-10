efa15ffa432c4be560070e6a9b136f72819fde7d
Tom Raviv <tomr@wix.com>
2023-04-10 04:09:02 +0300

    fix: improve general layout - change to simpler namespaces

--- stylable.config.js
+++ stylable.config.js
@@ -1,11 +1,25 @@
 //@ts-check
-const { typedConfiguration } = require('@stylable/cli');
+const { stcConfig } = require('@stylable/cli');
 
-exports.stcConfig = typedConfiguration({
-    options: {
-        srcDir: './src',
-        outDir: './st-types',
-        dts: true,
-        cjs: false,
+const { createNamespaceStrategyNode } = require('@stylable/node');
+
+module.exports = {
+    defaultConfig() {
+        return {
+            // set a custom namespace resolver
+            resolveNamespace: createNamespaceStrategyNode({
+                prefix: 'st_',
+                hashFragment: 'minimal',
+                strict: true,
+            }),
+        };
     },
-});
+    stcConfig: stcConfig({
+        options: {
+            srcDir: './src',
+            outDir: './st-types',
+            dts: true,
+            cjs: false,
+        },
+    }),
+};
